b8baa2896fbeabbce5cdd76cfe9e8d042eb72003
hrederik <32139140+hrederik@users.noreply.github.com>
2022-05-29 14:23:52 +0300

    Add spawn point

---
+++ Assets/_Project/CodeBase/Editor/SpawnPointEditor.cs
@@ -0,0 +1,17 @@
+using CodeBase.Spawn;
+using UnityEditor;
+using UnityEngine;
+
+namespace _Project.CodeBase.Editor
+{
+    [CustomEditor(typeof(SpawnPoint))]
+    public class SpawnPointEditor : UnityEditor.Editor
+    {
+        [DrawGizmo(GizmoType.Active | GizmoType.Pickable | GizmoType.NonSelected)]
+        public static void RenderGizmo(SpawnPoint point, GizmoType type)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(point.Transform.position, radius: 0.5f);
+        }
+    }
+}
