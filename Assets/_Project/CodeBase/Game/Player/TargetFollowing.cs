c84eb9e14db30fce480a4e400c5e05e17263f537
hrederik <32139140+hrederik@users.noreply.github.com>
2022-05-29 10:27:54 +0300

    Change game orientation to vertical

--- Assets/_Project/CodeBase/Game/Player/TargetFollowing.cs
+++ Assets/_Project/CodeBase/Game/Player/TargetFollowing.cs
@@ -8,15 +8,17 @@ namespace CodeBase.Game.Player
         [SerializeField] private float _speed;
         [SerializeField] private Transform _target;
         private Transform _transform;
+        private float _offset;
 
         private void Awake()
         {
             _transform = GetComponent<Transform>();
+            _offset = _transform.position.x - _target.position.x;
         }
 
         private void Update()
         {
-            Vector3 newPosition = new Vector3(_target.position.x, _transform.position.y, _transform.position.z);
+            Vector3 newPosition = new Vector3(_target.position.x + _offset, _transform.position.y, _transform.position.z);
             _transform.position = Vector3.MoveTowards(_transform.position, newPosition, _speed * Time.deltaTime);
         }
     }
