07c61533e15e256450033dab281744966c791822
hrederik <32139140+hrederik@users.noreply.github.com>
2022-05-29 09:46:03 +0300

    Import utilities

---
+++ Assets/_Project/CodeBase/Utilities/InterfaceValidator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+    public class InterfaceValidator : MonoBehaviour
+    {
+        public static void TryToValidate<T>(MonoBehaviour checkable)
+        {
+            if (checkable == null) return;
+            if (checkable is T == false) 
+                Debug.LogError($"{checkable.name} needs to implement {typeof(T).Name}");
+        }
+    }
+}
---
+++ Assets/_Project/CodeBase/Utilities/Physics/SpecificTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Utilities.Physic
+{
+    public abstract class SpecificTrigger<T> : MonoBehaviour
+    {
+        public event UnityAction<T> Entered;
+        public event UnityAction<T> Exited;
+        
+
+        private void OnTriggerEnter(Collider other)
+        {
+            InvokeIfTarget(other, Entered);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            InvokeIfTarget(other, Exited);
+        }
+
+        private void InvokeIfTarget(Collider other, UnityAction<T> callback)
+        {
+            var target = other.GetComponent<T>();
+
+            if (target != null) 
+                callback?.Invoke(target);
+        }
+    }
+}
