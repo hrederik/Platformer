d18130a4044b6271e100a00da3b390a0479b8ff9
hrederik <32139140+hrederik@users.noreply.github.com>
2022-05-29 15:22:19 +0300

    Change spikes animation to tween

--- Assets/_Project/CodeBase/Game/Level/Traps/Spikes.cs
+++ Assets/_Project/CodeBase/Game/Level/Traps/Spikes.cs
@@ -1,29 +1,38 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
 using UnityEngine;
 
 namespace CodeBase.Game.Level.Traps
 {
     public class Spikes : MonoBehaviour
     {
-        [SerializeField] private Animator _animator;
-        [SerializeField] private float _showSpikesDelay;
-        [SerializeField] private float _hideSpikesDelay;
+        [SerializeField] private float _showDelay = 2;
+        [SerializeField] private float _showDuration = 1.0f;
+        [SerializeField] private Ease _show;
+        [Space]
+        [SerializeField] private float _hideDelay = 0.5f;
+        [SerializeField] private float _hideDuration = 0.5f;
+        [SerializeField] private Ease _hide;
+
+        [Space]
+        [SerializeField] private float _showedPosition;
+        [SerializeField] private float _hidPosition;
+        [SerializeField] private Transform _spikes;
 
         private void Start()
         {
-            StartCoroutine(ActivateTrap());
-        }
-
-        private IEnumerator ActivateTrap()
-        {
-            while (true)
-            {
-                yield return new WaitForSeconds(_showSpikesDelay);
-                _animator.SetTrigger("ShowSpikes");
-
-                yield return new WaitForSeconds(_hideSpikesDelay);
-                _animator.SetTrigger("HideSpikes");
-            }
+            var sequence = DOTween.Sequence();
+            sequence
+                .AppendInterval(_showDelay)
+                .Append(
+                    _spikes
+                        .DOMoveY(_showedPosition, _showDuration)
+                        .SetEase(_show))
+                .AppendInterval(_hideDelay)
+                .Append(
+                    _spikes
+                        .DOMoveY(_hidPosition, _hideDuration)
+                        .SetEase(_hide))
+                .SetLoops(-1);
         }
     }
 }
